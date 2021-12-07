f730510dff14eb51bdc8967208ef5ba73e1447ec
zed.zhai <zed.zhai@robosense.cn>
2021-12-07 20:50:01 +0800

    save

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,19 @@
 #include "thread_pool.h"
+#include <iostream>
+#include <chrono>
+
+std::mutex g_mutex;
+
+void task(int i) {
+    std::cout << "正在执行第 " << i << " 个任务" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    std::cout << "执行完成第 " << i << " 个任务" << std::endl;
+}
 
 int main(int argc, char** argv) {
     // todo:
+    ThreadPool::Ptr thread_pool_ptr = std::make_shared<ThreadPool>(1);
+    for (int i = 0; i < 1; ++i) {
+        thread_pool_ptr->commit([i](){ task(i); });
+    }
 }
--- thread_pool.h
+++ thread_pool.h
@@ -1,1 +1,76 @@
-#
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <atomic>
+#include <future>
+#include <functional>
+
+
+class ThreadPool {
+public:
+    using Ptr = std::shared_ptr<ThreadPool>;
+
+    ThreadPool(int idl_thr_num) : run_(true), idl_thr_num_(idl_thr_num) {
+        for (int i = 0; i < idl_thr_num_; ++i) {
+            pool_.emplace_back([this]{  // 工作线程函数
+                while (run_) {
+                    Task task;
+                    {
+                        std::unique_lock<std::mutex> lock{ mutex_ };
+                        cv_.wait(lock, [this]{ return !run_ || !task_.empty(); });  // 等待直到任务队列有任务或者线程池停止工作
+                        if (!run_ && task_.empty()) {
+                            return;
+                        }
+                        task = std::move(task_.front());  // 从任务队列首取出一个任务
+                        task_.pop();
+                    }
+                    idl_thr_num_--;
+                    task();  // 执行任务
+                    idl_thr_num_++;
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        run_ = false;
+        cv_.notify_all();
+        for (std::thread& thread : pool_) {
+            if (thread.joinable()) {
+                thread.join();
+            }
+        }
+    }
+
+public:
+    int idlCount() {
+        return idl_thr_num_;
+    }
+
+    template <class F, class... Args>
+    auto commit(F&& f, Args&&... args)->std::future<decltype(f(args...))> {
+        if (!run_) {
+            throw std::runtime_error("commit on ThreadPool is stopped.");
+        }
+        using RetType = decltype(f(args...));  // 函数f的返回值类型
+        auto task = std::make_shared<std::packaged_task<RetType()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        std::future<RetType> future = task->get_future();
+        {
+            std::lock_guard<std::mutex> lock{mutex_ };
+            task_.emplace([task](){ (*task)(); });  // 添加任务到任务队列
+        }
+        cv_.notify_one();  // 唤醒一个线程
+        return future;
+    }
+
+private:
+    using Task = std::function<void()>;
+    std::vector<std::thread> pool_;             // 线程池
+    std::queue<Task> task_;                     // 任务队列
+    std::mutex mutex_;                          // 线程锁
+    std::condition_variable cv_;                // 条件阻塞
+    std::atomic<bool> run_;                     // 线程池是否执行标志
+    std::atomic<int> idl_thr_num_;              // 空闲线程数量
+};
